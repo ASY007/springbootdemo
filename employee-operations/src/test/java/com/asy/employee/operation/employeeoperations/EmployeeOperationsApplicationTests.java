8029fdf6c7bc06d5994274781ef2cc24dc597940
ASY <ASY@ASY-PC>
2019-10-17 00:51:41 +0530

    initial commit

---
+++ employee-operations/src/test/java/com/asy/employee/operation/employeeoperations/EmployeeOperationsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.asy.employee.operation.employeeoperations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeOperationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
